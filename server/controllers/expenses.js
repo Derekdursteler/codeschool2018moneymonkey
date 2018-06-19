e5234511f174c5bbeabc12e50de68ac648707473
Andrew Eames <aeames@CIT-SCCT12-1206.cs.dixie.edu>
2018-06-19 11:02:12 -0600

    Controller/router layout.

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -1,3 +1,6 @@
 module.exports = {
-	listExpenses: (req, res) => res.send('list expenses')
+	listExpenses: (req, res) => res.send('list expenses'),
+	createExpense: (req, res) => res.send('created expense'),
+	updateExpense: (req, res) => res.send('updated expense'),
+	deleteExpense: (req, res) => res.send('deleted expense')
 }
--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -3,8 +3,20 @@ const router = express.Router()
 
 const expenseController = require('../controllers/expenses')
 
+// COLLECTION PATHS
+
 // a get to '/expenses'
 router.get('/', expenseController.listExpenses)
+// a post to '/expenses'
+router.post('/', expenseController.createExpense)
+
+
+// ELEMENT PATHS
+
+// a put to '/expenses/id'
+router.put('/:id', expenseController.updateExpense)
+// a delete to '/expenses/id'
+router.delete('/:id', expenseController.deleteExpense)
 
 // export default router
 module.exports = router
