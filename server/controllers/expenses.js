4876f828d5a7b43e76ab0d67d1c40b0cd0292ba3
Andrew Eames <aeames@CIT-SCCT12-1206.cs.dixie.edu>
2018-06-19 12:58:39 -0600

    Modifying expenses.

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -1,9 +1,32 @@
+const moment = require('moment')
+
 module.exports = {
 	listExpenses: (req, res) => res.json(expenses),
 	createExpense: (req, res) => {
-		console.log(req.body)
+		const expense = {
+			id: Math.random(),
+			description: req.body.description,
+			amount: req.body.amount,
+			quantity: req.body.quantity,
+			date: moment().format('MMMM Do, YYYY')
+		}
+		expenses.unshift(expense)
 		res.status(201).send('created expense')
 	},
-	updateExpense: (req, res) => res.send('updated expense'),
-	deleteExpense: (req, res) => res.status(204)
+	updateExpense: (req, res) => {
+		const indexOfExpense = expenses.findIndex(expense => expense.id == req.params.id)
+		const updatedExpense = {
+			id: expenses[indexOfExpense].id,
+			description: req.body.description,
+			amount: req.body.amount,
+			quantity: req.body.quantity,
+			date: moment().format('MMMM Do, YYYY')
+		}
+		expenses.splice(indexOfExpense, 1, updatedExpense)
+		res.json(updatedExpense)
+	},
+	deleteExpense: (req, res) => {
+		expenses = expenses.filter(expense => expense.id != req.params.id)
+		res.status(204).send()
+	},
 }
