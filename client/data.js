98d113a44aba7477eaec99034f62c27dde9e934b
Andrew Eames <aeames@CIT-SCCT12-1206.cs.dixie.edu>
2018-06-22 12:57:46 -0600

    Intial vuetify.

--- client/data.js
+++ client/data.js
@@ -8,7 +8,38 @@ const data = {
 		description: true,
 		amount: true
 	},
-	expenses: []
+	expenses: [],
+	headers: [
+	{
+		text: 'Description',
+		align: 'left',
+		sortable: false,
+		value: 'description'
+	},
+	{
+		text: 'Amount',
+		align: 'left',
+		sortable: true,
+		value: 'amount'
+	},
+	{
+		text: 'QTY',
+		align: 'left',
+		sortable: true,
+		value: 'quantity'
+	},
+	{
+		text: 'Date',
+		align: 'left',
+		sortable: true,
+		value: 'date'
+	},
+	{
+		text: 'Actions',
+		align: 'left',
+		value: ''
+	},
+	]
 }
 
 export default data
